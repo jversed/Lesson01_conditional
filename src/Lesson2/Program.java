a91e1f0f32534cbe451770938708f5c386817093
Alexandr Slobodnichenko <aslobodnichenko@gmail.com>
2018-08-15 23:33:04 +0300

    comments added

--- src/Lesson2/Program.java
+++ src/Lesson2/Program.java
@@ -4,10 +4,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO finish program
-
-		int x = 2;
+		int a,b,c; // declares three ints a b c
+		
+		int x = 2; // declares x int and initializing it
 		int y = 3;
-		int res = x + 1;
+		int res = x + 1; // dynamically initialized
 		System.out.println(res);
 		
 		String str1="vas";
@@ -19,10 +20,18 @@ public class Program {
 		System.out.println(str11==str22);
 		System.out.println(str11.equals(str22));
 		
-		char ch = '9'; 
+		char ch = '9'; // variable ch has the value '9'
 		int i1 = Character.getNumericValue(ch);    
 		System.out.println(i1); 
 		System.out.println(1+i1);
+		
+		int oct = 056;
+		System.out.println("octal = "+oct);
+		int hex = 0xAB;
+		System.out.println("hexadecimal = "+hex);
+		
+		int undrscrs = 435_244__123;
+		System.out.println(undrscrs); //underscores will be ignored
 		
 		
 		
